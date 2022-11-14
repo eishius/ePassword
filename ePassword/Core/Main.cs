bd3e24cd7310dd11a643c234fd95dc933c415c3f
Snoopy <118087017+eishius@users.noreply.github.com>
2022-11-13 22:31:21 -0600

    fixed

--- ePassword/Core/Main.cs
+++ ePassword/Core/Main.cs
@@ -7,41 +7,28 @@ using System.Text;
 
 namespace ePassword.Core
 {
- 
+    // Developed by Eishius.
     internal class Main
     {
         internal static void Startup()
         {
             Thread.Sleep(4000);
-            Build();
-            Console.ReadLine();
-        }
-
-        internal static void Build()
-        {
             Console.Clear();
             Thread.Yield();
             Con.Write("User: " + Environment.UserName);
             Thread.Sleep(2000);
             BuildPasswords();
+            Console.ReadLine();
         }
-        static void OpenFile(string[] args)
-        {
-            if (args.Length == 0) { Console.ReadLine(); }
-            string str = Console.ReadLine(); ;
-            string path = "documents/ePasswords/passwords.txt";
-            Con.Write("Would you like to save these passwords?");
-            if (str == "false" || str == "") { Con.Write("Press Enter!"); Console.ReadLine(); }
-            Con.Write("Saving Passwords to" + path + ".");
-        }
-
         async static void BuildPasswords()
         {
             int PasswordAmount = 0;
             bool v = false;
             Con.Write(Environment.UserName + " How many should we make?");
             PasswordAmount = int.Parse(Console.ReadLine());
+            Thread.Sleep(1000);
             Con.Write("Creating Passwords...");
+            Console.Clear();
             string[] AllPasswords = new string[PasswordAmount];
             for (int i = 0; i < PasswordAmount; i++)
             {
